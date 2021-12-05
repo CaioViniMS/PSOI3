fce963b53eb1eefa2a32c678300c4979746f8dee
Caio Vinicius Martins da Silva <Contato.caiovini@gmail.com>
2021-12-05 12:26:37 -0300

    PSOI3

--- PreP3/src/view/Principal.java
+++ PreP3/src/view/Principal.java
@@ -4,26 +4,22 @@ import controller.Faculdades;
 
 public class Principal {
 
-	
 	public final static String os = System.getProperty("os.name");
-	
+
 	public static void main(String[] args) {
-		
-		
-		       
-		       
-				if(os.contains("Windows")){
-					    String dir = "C:\\TEMP";
-					    Faculdades.criaDirArq(dir);
-					    Faculdades.lerArquivo(dir);
-						}else{
-						      if(os.contains("Linux")) { 
-							   String dir = "/tmp";
-							   Faculdades.criaDirArq(dir);
-							    Faculdades.lerArquivo(dir);
-						      }
-							}
-		
+
+		if (os.contains("Windows")) {
+			String dir = "C:\\TEMP";
+			Faculdades.criaDirArq(dir);
+			Faculdades.lerArquivo(dir);
+		} else {
+			if (os.contains("Linux")) {
+				String dir = "/tmp";
+				Faculdades.criaDirArq(dir);
+				Faculdades.lerArquivo(dir);
+			}
+		}
+
 	}
 
 }
